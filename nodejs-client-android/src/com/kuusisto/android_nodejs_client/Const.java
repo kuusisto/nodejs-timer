5bcdca96d188f1744dc41eb73491ab9c771ef1cd
Jari Kuusisto <jari@nexus.fi>
2013-11-07 17:09:16 +0200

    proper server address

--- nodejs-client-android/src/com/kuusisto/android_nodejs_client/Const.java
+++ nodejs-client-android/src/com/kuusisto/android_nodejs_client/Const.java
@@ -2,7 +2,7 @@ package com.kuusisto.android_nodejs_client;
 
 public class Const {
 
-    private static final String SERVER = "http://pvsf.pv.com";
+    private static final String SERVER = "http://localhost";
 
     public static String getServer() {
         return SERVER;
